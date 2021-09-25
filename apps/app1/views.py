3f1ceaa594eb4998445fdc78d3f5fd9eaafb5be0
Grinch101 <masoud.kameli@gmail.com>
2021-09-25 18:33:46 -0400

    edit urls

--- apps/app1/views.py
+++ apps/app1/views.py
@@ -1,6 +1,7 @@
 import json
 from json.encoder import JSONEncoder
 from django import http
+from django.db.models import fields
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
@@ -12,7 +13,8 @@ from django.core.serializers import serialize
 
 @api_view(['POST', 'GET'])
 def index(req):
-    
+    if req.method == 'GET':
+        return HttpResponse('You can POST to this URL to save your message. \n acceptale format: {"title":"YourTitle" , "body":"Your Text Body"} \n to see all records see /getall ')
     body = json.loads(req.body)
     title = body['title']
     text = body['body']
@@ -24,7 +26,7 @@ def index(req):
 @api_view(['GET'])
 def get_all(req):
     res = Blog.objects.all()
-    res = serialize('json', res)
-    res = json.loads(res)
-    print(res)
-    return JsonResponse(res, safe=False)
+    # res = serialize('json', res)
+    # res = json.loads(res)
+    res = [ x['fields'] for x in res ] 
+    return JsonResponse("res", safe=False)
